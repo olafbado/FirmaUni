ef5939d5059114abbb275c52f728b9cc5e973284
olafbado <niebiel1@gmail.com>
2025-05-06 19:46:14 +0100

    Implement CRUD functionality for Towar

--- Firma.Intranet/Controllers/TowarController.cs
+++ Firma.Intranet/Controllers/TowarController.cs
@@ -1,4 +1,5 @@
 ﻿using Firma.Data.Data;
+using Firma.Data.Data.Sklep;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,7 +16,76 @@ public class TowarController : Controller
 
     public async Task<IActionResult> Index()
     {
-        var firmaIntranetContext = _context.Towar.Include(t => t.Rodzaj);
-        return View(await firmaIntranetContext.ToListAsync());
+        return View(await _context.Towar.Include(t => t.Rodzaj).ToListAsync());
+    }
+
+    public async Task<IActionResult> Details(int id)
+    {
+        var towar = await _context
+            .Towar.Include(t => t.Rodzaj)
+            .FirstOrDefaultAsync(t => t.IdTowaru == id);
+        if (towar == null)
+            return NotFound();
+        return View(towar);
+    }
+
+    public IActionResult Create()
+    {
+        ViewBag.Rodzaje = _context.Rodzaj.ToList();
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Towar towar)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(towar);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        ViewBag.Rodzaje = _context.Rodzaj.ToList();
+        return View(towar);
+    }
+
+    public async Task<IActionResult> Edit(int id)
+    {
+        var towar = await _context.Towar.FindAsync(id);
+        if (towar == null)
+            return NotFound();
+        ViewBag.Rodzaje = _context.Rodzaj.ToList();
+        return View(towar);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, Towar towar)
+    {
+        if (id != towar.IdTowaru)
+            return NotFound();
+
+        if (ModelState.IsValid)
+        {
+            _context.Update(towar);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        ViewBag.Rodzaje = _context.Rodzaj.ToList();
+        return View(towar);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var towar = await _context.Towar.FindAsync(id);
+        if (towar != null)
+        {
+            _context.Towar.Remove(towar);
+            await _context.SaveChangesAsync();
+        }
+        return RedirectToAction(nameof(Index));
     }
 }
