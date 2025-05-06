d2c2d4e3d5c289cd988f34a69f0f27d87fcb8cee
olafbado <niebiel1@gmail.com>
2025-05-06 18:54:16 +0100

    Implement CRUD operations for Aktualnosc

--- Firma.Intranet/Controllers/AktualnoscController.cs
+++ Firma.Intranet/Controllers/AktualnoscController.cs
@@ -1,4 +1,5 @@
 ﻿using Firma.Data.Data;
+using Firma.Data.Data.CMS;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,5 +17,68 @@ public class AktualnoscController : Controller
     public async Task<IActionResult> Index()
     {
         return View(await _context.Aktualnosc.ToListAsync());
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Aktualnosc aktualnosc)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(aktualnosc);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(aktualnosc);
+    }
+
+    public async Task<IActionResult> Edit(int id)
+    {
+        var aktualnosc = await _context.Aktualnosc.FindAsync(id);
+        if (aktualnosc == null)
+            return NotFound();
+        return View(aktualnosc);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, Aktualnosc aktualnosc)
+    {
+        if (id != aktualnosc.IdAktualnosci)
+            return NotFound();
+
+        if (ModelState.IsValid)
+        {
+            _context.Update(aktualnosc);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(aktualnosc);
+    }
+
+    public async Task<IActionResult> Details(int id)
+    {
+        var aktualnosc = await _context.Aktualnosc.FindAsync(id);
+        if (aktualnosc == null)
+            return NotFound();
+        return View(aktualnosc);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var aktualnosc = await _context.Aktualnosc.FindAsync(id);
+        if (aktualnosc != null)
+        {
+            _context.Aktualnosc.Remove(aktualnosc);
+            await _context.SaveChangesAsync();
+        }
+        return RedirectToAction(nameof(Index));
     }
 }
