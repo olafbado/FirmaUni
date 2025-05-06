4e567172bc9ee612a2bf8096c100ae36081b7c04
olafbado <niebiel1@gmail.com>
2025-05-06 18:44:21 +0100

    Add CRUD functionality for Strona

--- Firma.Intranet/Controllers/StronaController.cs
+++ Firma.Intranet/Controllers/StronaController.cs
@@ -1,4 +1,5 @@
 ﻿using Firma.Data.Data;
+using Firma.Data.Data.CMS;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,5 +17,105 @@ public class StronaController : Controller
     public async Task<IActionResult> Index()
     {
         return View(await _context.Strona.ToListAsync());
+    }
+
+    // GET: Strona/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Strona/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Strona strona)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(strona);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(strona);
+    }
+
+    // GET: Strona/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+            return NotFound();
+
+        var strona = await _context.Strona.FindAsync(id);
+        if (strona == null)
+            return NotFound();
+
+        return View(strona);
+    }
+
+    // POST: Strona/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, Strona strona)
+    {
+        if (id != strona.IdStrony)
+            return NotFound();
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(strona);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Strona.Any(e => e.IdStrony == id))
+                    return NotFound();
+                else
+                    throw;
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(strona);
+    }
+
+    // GET: Strona/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+            return NotFound();
+
+        var strona = await _context.Strona.FirstOrDefaultAsync(m => m.IdStrony == id);
+        if (strona == null)
+            return NotFound();
+
+        return View(strona);
+    }
+
+    // GET: Strona/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+            return NotFound();
+
+        var strona = await _context.Strona.FirstOrDefaultAsync(m => m.IdStrony == id);
+        if (strona == null)
+            return NotFound();
+
+        return View(strona);
+    }
+
+    // POST: Strona/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var strona = await _context.Strona.FindAsync(id);
+        if (strona != null)
+        {
+            _context.Strona.Remove(strona);
+            await _context.SaveChangesAsync();
+        }
+        return RedirectToAction(nameof(Index));
     }
 }
